bd1f773fcfcf58ea049ed5f6923211552ad02d7e
Arnorld Omoja <omojaarnorld7@gmail.com>
2026-02-04 19:34:24 +0300

    Fix Firebase init to load credentials from env JSON

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -2,15 +2,17 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
-from pathlib import Path
+import json
 
 service_account = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
 if not service_account:
     raise RuntimeError("FIREBASE_SERVICE_ACCOUNT not found in environment")
 
+# Initialize Firebase only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(Path(service_account))
+    cred_dict = json.loads(service_account)
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 def verify_token(id_token: str):
